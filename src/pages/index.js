c81144b0f4267717e90a3a78fdfd6c0096c3568c
keremcanb <keremcan@gmail.com>
2020-12-13 00:48:04 +0300

    home projects query

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,39 @@ import {
 } from '../components';
 import SEO from '../components/Seo/seo';
 
-const HomePage = () => (
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 3
+      sort: { fields: data___date, order: DESC }
+    ) {
+      nodes {
+        id
+        data {
+          date
+          name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const HomePage = ({
+  data: {
+    allAirtable: { nodes: projects }
+  }
+}) => (
   <Layout>
     <Hero />
     <About />
