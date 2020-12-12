7f0b1db0fe2af744ac571fb574a43ad588063e36
keremcanb <keremcan@gmail.com>
2020-12-12 20:19:04 +0300

    global styles root wrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/Layout/layout';
-import Hero from '../components/Hero/Hero';
-import About from '../components/About/About';
-import Projects from '../components/Projects/Projects';
-import Survey from '../components/Survey/Survey';
-import Slider from '../components/Slider/Slider';
-import GridProjects from '../components/GridProjects/GridProjects';
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects
+} from '../components';
+import SEO from '../components/Seo/seo';
+// import Layout from '../components/Layout/layout';
+// import Hero from '../components/Hero/Hero';
+// import About from '../components/About/About';
+// import Projects from '../components/Projects/Projects';
+// import Survey from '../components/Survey/Survey';
+// import Slider from '../components/Slider/Slider';
+// import GridProjects from '../components/GridProjects/GridProjects';
 
 const HomePage = () => (
   <Layout>
