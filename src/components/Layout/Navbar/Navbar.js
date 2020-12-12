e1b99e2bc2c74c60cb9e7629c6073cca0fc772d6
keremcanb <keremcan@gmail.com>
2020-12-12 22:17:20 +0300

    navbar

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -5,6 +5,32 @@ import logo from '../../../images/logo.svg';
 import NavLink from '../NavLink/NavLink';
 import { Wrapper } from './styles';
 
-const Navbar = () => <h2>navbar component</h2>;
+const Navbar = () => (
+  <Wrapper>
+    <div className="nav-center">
+      <div className="nav-header">
+        <Link to="/">
+          <img src={logo} alt="design" />
+        </Link>
+
+        <button className="toggle-btn">
+          <GoThreeBars />
+        </button>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <button>products</button>
+        </li>
+        <li>
+          <button>devs</button>
+        </li>
+        <li>
+          <button>company</button>
+        </li>
+      </ul>
+    </div>
+  </Wrapper>
+);
 
 export default Navbar;
